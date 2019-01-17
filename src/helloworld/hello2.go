5d39878121307f68fde361eef9ab4b952a61a5ac
wangruidong <wangrd14@chinaunicom.cn>
2019-01-17 16:02:35 +0800

    引入jar包文件例子

---
+++ src/helloworld/hello2.go
@@ -0,0 +1,5 @@
+package helloworld
+
+func Aaa() {
+	println(1 + 1)
+}
--- src/helloworld/main/main.go
+++ src/helloworld/main/main.go
@@ -9,7 +9,9 @@ package main
  */
 import (
 	"fmt"
+	"helloworld"
 )
+
 /**
  *	下一行 func main() 是程序开始执行的函数。main 函数是每一个可执行程序所必须包含的，一般来说都是在启动后第一个执行的函数（如果有 init() 函数则会先执行该函数）
  *  当标识符（包括常量、变量、类型、函数名、结构字段等等）以一个大写字母开头，如：Group1，那么使用这种形式的标识符的对象就可以被外部包的代码所使用（客户端程序需要先导入这个包），这被称为导出（像面向对象语言中的 public）；标识符如果以小写字母开头，则对包外是不可见的，但是他们在整个包的内部是可见并且可用的（像面向对象语言中的 protected ）
@@ -19,5 +21,7 @@ func main() {
 	fmt.Println("Hello, World!")
 	fmt.Println("this is a test program")
 	//GO语言中末尾可以省略分号 “;”，编译器会自动加上，但是如果俩个语句写在同一行，则必须人为加上
-	fmt.Print("在一行的话,");fmt.Println("必须加上分号")
+	fmt.Print("在一行的话,")
+	fmt.Println("必须加上分号")
+	helloworld.Aaa()
 }
